7e4e027e01ccb7d9df09ecace29cc18ecc599988
ShivanshiSingh08 <141561870+ShivanshiSingh08@users.noreply.github.com>
2024-01-17 14:10:29 +0530

    Add files via upload

---
+++ Javalab10/src/javalab10/Javalab10.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package javalab10;
+
+/**
+ *
+ * @author hp
+ */
+public class Javalab10 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        new BusTicketBooking().setVisible(true);
+    }
+    
+}
